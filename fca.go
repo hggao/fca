de8222507b367ebb6861f527674bfdff07be0138
Bruce Gao <bruce.gao@nutanix.com>
2018-02-28 15:32:13 -0800

    Add command line args and usage

--- fca.go
+++ fca.go
@@ -3,11 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	n_worker := flag.Int("workers", 1, "Number of workers")
 	flag.Parse()
+	flag.Usage = func() {
+		fmt.Println("Usage:\n ", os.Args[0], "file_path [OPTIONS]")
+		fmt.Println("\nOPTIONS:")
+		flag.PrintDefaults()
+		fmt.Println("\nExample:")
+		fmt.Println(" ", os.Args[0], "test.raw --workers=8\n")
+	}
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	fmt.Println("Number of workers: ", *n_worker)
 }
