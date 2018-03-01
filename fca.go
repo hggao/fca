f92d9dc09b9e6fc440503ea20ad67e3afcf79c9b
Bruce Gao <bruce.gao@nutanix.com>
2018-02-28 16:18:09 -0800

    Fix args and usage problem

--- fca.go
+++ fca.go
@@ -10,16 +10,18 @@ func main() {
 	n_worker := flag.Int("workers", 1, "Number of workers")
 	flag.Parse()
 	flag.Usage = func() {
-		fmt.Println("Usage:\n ", os.Args[0], "file_path [OPTIONS]")
+		fmt.Println("Usage:\n ", os.Args[0], "[OPTIONS] file")
 		fmt.Println("\nOPTIONS:")
 		flag.PrintDefaults()
 		fmt.Println("\nExample:")
-		fmt.Println(" ", os.Args[0], "test.raw --workers=8\n")
+		fmt.Println(" ", os.Args[0], "-workers=8 test.raw\n")
 	}
-	if flag.NArg() == 0 {
+	if flag.NArg() != 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
+	file_path := flag.Args()[0]
 
-	fmt.Println("Number of workers: ", *n_worker)
+	fmt.Println("File to analysis:", file_path)
+	fmt.Println("Number of workers:", *n_worker)
 }
